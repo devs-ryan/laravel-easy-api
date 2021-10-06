7f5cfd90e8e8aa6b382b688fc469f6bc0753b84d
devs_ryan <rclaxton.dev@gmail.com>
2021-10-05 19:05:54 -0600

    fix routes

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,6 @@ Route::group(['middleware' => ['api'], 'prefix' => env('EASY_API_BASE_URL', 'eas
     Route::get('/', 'AdminController@home');
     Route::get('{model}/index', 'AdminController@index');
     Route::post('{model}', 'AdminController@store');
-    Route::put('{model}/{id}', 'AdminController@update');
-    Route::patch('{model}/{id}', 'AdminController@update');
+    Route::match(['put', 'patch'], '{model}/{id}', 'AdminController@update');
     Route::delete('{model}/{id}', 'AdminController@destroy');
 });
